0856f2eb31ff84bd0895953d4de0fb2e7bf48dd9
Ramdeni Akshith <>
2022-08-22 06:42:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('This is your First JavaScript'); // Console.log() is Used to Print the Command.
+
+// variable are Used to Store  a value. and it is similar to the container used to store the data value like rice , water ,milks and etc
+
+let a =15;
+console.log(a);
