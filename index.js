2d64c9226e9f13001befc994af2bdacbd784d6d1
Ramdeni Akshith <108686558+AkshitRamdeni@users.noreply.github.com>
2022-08-22 13:54:58 +0000

    var let const

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ console.log('This is your First JavaScript'); // Console.log() is Used to Print 
 
 // variable are Used to Store  a value. and it is similar to the container used to store the data value like rice , water ,milks and etc
 
-let a =15; // a contains 15
+let a = 15; // a contains 15
 console.log(a);
 a = 'Akki';
 console.log(a) // print String
+
+// now lets the Differences btw let const var 
+
+// const = constants that we cannot be change the value throughout the Javascript.
+
+// var 
+
+var e = 11;
+var b = 'Akshith';
+var c = null;
+var d = undefined;
+console.log(e, b, c, d);
+
+// let 
+
+let m = 12;
+let n = 'akki';
+let o = null;
+let p = 0;
+
+console.log(m, n, o, p)
+
+
+// const
+
+const name = 'Cant be changed';
+let name_1 = 'Ramdeni';
+
+console.log(name, name_1)
